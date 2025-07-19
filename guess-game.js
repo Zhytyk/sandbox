5cc632628db60f88059ad55a42262e714fc3b9dd
paulvaidalauskas <zhytyk@gmail.com>
2025-07-19 13:40:34 +0300

    initial commit

---
+++ guess-game.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const guessMessageInfo = 'Please, guess the number: ';
+const fromZeroToTenRandomNumber = Math.random() * 10;
+const roundedRandomNumber = Math.round(fromZeroToTenRandomNumber);
+let hasGuessed = false;
+
+
+function askQuestion(query) {
+    return new Promise((resolve) => {
+        rl.question(query, resolve);
+    });
+}
+
+
+async function guessNumber() {
+    while (!hasGuessed) {
+        const enteredNumber = await askQuestion(guessMessageInfo);
+        if (Number(enteredNumber) === roundedRandomNumber) {
+            hasGuessed = true;
+            console.log('You guessed!');
+            rl.close();
+        } else {
+            console.log('You are wrong! Try again');
+        }
+    }
+}
+
+(async () => {
+    await guessNumber();
+})();
+
+
